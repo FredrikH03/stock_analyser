6298e85d875be2cc9563fa8810dcdcf6fb9f5213
veraly <git@veraly.zerv.cc>
2026-03-22 11:07:42 +0100

    function to handle parameters, disgusting but working

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -4,9 +4,13 @@
 #include <boost/json.hpp>
 #include <boost/url.hpp>
 #include <iostream>
+#include <bits/stdc++.h>
+// #include <map>
 
 using namespace boost::urls;
 using std::cout, std::endl;
+using namespace std;
+
 
 #include <iostream>
 
@@ -15,6 +19,34 @@ namespace http = beast::http;
 namespace net = boost::asio;
 namespace json = boost::json;
 using tcp = boost::asio::ip::tcp;
+
+void parse_params(std::map<string, string> *params_map, string url_str){
+  // Absolutely the most disgusting piece of code I've made, but it works.
+  string url_delimiter = "?";
+  string param_delimiter = "=";
+  size_t url_pos = 0;
+  string params = url_str.substr(url_pos = url_str.find(url_delimiter) + url_delimiter.length());
+  std::vector<std::string> tokens;
+  size_t pos = 0;
+  string token;
+  while ( true){
+    pos = params.find("&");
+    string delimiter = "&";
+    token = params.substr(0, pos);
+    if(token != "" && params.find("&") < 1000){
+      int test = 0;
+      (*params_map).insert({token.substr(0, token.find(param_delimiter)), token.substr(0 + token.find("=") + param_delimiter.length())});
+    }
+
+    params.erase(0, pos + delimiter.length());
+
+    if(params.find("&") > 1000){
+      (*params_map).insert({params.substr(0, params.find(param_delimiter)), params.substr(0 + params.find("=") + param_delimiter.length())});
+      params.erase(0, pos + delimiter.length());
+      break;
+    }
+  }
+}
 
 // actual request handler, generates json response from http request
 void handle_request(const http::request<http::string_body>& req, http::response<http::string_body>& res){
@@ -26,6 +58,18 @@ void handle_request(const http::request<http::string_body>& req, http::response<
     url_view parsed_url(req.target());
     auto params = parsed_url.params();
     auto path = parsed_url.path();
+
+    string url_str(req.target());
+
+    std::cout << "useful info " << url_str << '\n';
+
+    map<string, string> params_map;
+    parse_params(&params_map, url_str);
+
+    // map<string, string> params_map;
+    // parse_params(&params_map, params.to_string());
+    
+
     // basically whats after the port (like /api/auth?username=USERNAME&password=PASSWORD) 
 
     if(path == "/hello"){
@@ -64,16 +108,18 @@ void handle_request(const http::request<http::string_body>& req, http::response<
     } else if (path == "/api/stocks"){
       json::object json_response;
       json_response["status"] = "200";
+      json_response["ticker"] = params_map["ticker"];
 
 
-      url_view u( "?Firstname=John&Last=Doe&last=test" );
-      
-      test = u.params().find( "last" ) != u.params().find( "last", ignore_case ) ;
+      // url_view u = parsed_url.params();
+
+      // std::cout << "connection to '127.0.0.1:8080/test' received with params; " << p << '\n';
+      // auto test = params.params().find( "ticker" ) != u.params().find( "ticker", ignore_case ) ;
 
       // string ticker;
       // ticker == 
       // std::cout << "connection to '127.0.0.1:8080/api/stocks' received with params; " << params << '\n';
-      std::cout << "connection to '127.0.0.1:8080/api/stocks' received, ; " << test << '\n';
+      // std::cout << "connection to '127.0.0.1:8080/api/stocks' received, ; " << test << '\n';
 
       res.result(http::status::ok);
       res.set(http::field::content_type, "application/json");
